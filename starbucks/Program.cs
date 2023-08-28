22cbfdb03496a037cb54bb0e08d256305afc734d
Dhruvi Rajput <133660516+DhruviRajput12@users.noreply.github.com>
2023-08-28 14:29:30 -0400

    Add files via upload

---
+++ starbucks/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("*****************************************************");
+Console.WriteLine("             Welcome to Starbucks            ");
+Console.WriteLine("             Address: 15 St.RoseMerry Road             ");
+Console.WriteLine("             Contact Number: +1 476-974-980             ");
+Console.WriteLine("             Store Number: 4968             ");
+Console.WriteLine("*****************************************************");
+
+
+
+// calling facade method to simplify UI/UX, order is a facade class
+Order.GetOrder();
+
+Console.WriteLine("\n >>> Thanks for ordering with Starbucks.\n >>> Please visit again!");
